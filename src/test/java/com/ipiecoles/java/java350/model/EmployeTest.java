1104f663c7c7e17de42114b552018a464417e30f
antoinebouard <antoinebouardain@gmail.com>
2022-01-19 08:26:19 +0100

    Add parameterized test for method getPrimeAnnuelle in Employe.java

--- src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
+++ src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 import java.time.LocalDate;
 
-public class EmployeTest {
+class EmployeTest {
 
     @Test
     public void getNombreAnneeAncienneteNow(){
@@ -55,5 +55,32 @@ public class EmployeTest {
 
         //Then
         Assertions.assertEquals(0, anneeAnciennete.intValue());
+    }
+
+    @ParameterizedTest(name = "Matricule {0}, performance {1}, anciennete {2}, temps partiel {3} => prime {4}")
+    @CsvSource({
+            "'T12345',1,0,1.0,1000.0",
+            "'T12345',1,0,0.5,500.0",
+            "'T12345',1,2,1.0,1200.0",
+            ",1,0,1.0,1000.0",
+            "'T12345',,0,1.0,1000.0",
+            "'M12345',1,0,1.0,1700.0",
+            "'M12345',1,3,1.0,2000.0",
+            "'T12345',2,0,1.0,2300.0",
+            "'T12345',2,1,1.0,2400.0",
+    })
+    void testGetPrimeAnnuelle(String matricule, Integer performance, Long nbAnneesAnciennete, Double tempsPartiel, Double primeAttendue) {
+        // Given
+        Employe employe = EmployeMaker.employeTechnicienPleinTemps().
+                but().withMatricule(matricule).
+                but().withPerformance(performance).
+                but().withDateEmbauche(LocalDate.now().minusYears(nbAnneesAnciennete)).
+                but().withTempsPartiel(tempsPartiel).
+                build();
+        // When
+        Double prime = employe.getPrimeAnnuelle();
+        // Then
+        // Prime de base + prime de perf + prime d'ancienneté au pro rata de son activité
+        Assertions.assertEquals(prime, primeAttendue);
     }
 }
