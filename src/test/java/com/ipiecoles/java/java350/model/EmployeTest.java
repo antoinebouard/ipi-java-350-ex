814fb823e91e8ca89ac99caf6e27982866f390df
antoinebouard <antoinebouardain@gmail.com>
2022-01-31 17:22:39 +0100

    fix test

--- src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
+++ src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
@@ -162,15 +162,15 @@ class EmployeTest {
         Assertions.assertEquals(0, e.getSalaire());
     }
 
-    @ParameterizedTest(name = "A l'annee {1}, pour un temps partiel de {2}, le nombre de RTT sera de => {3}")
+    @ParameterizedTest(name = "A l'annee {0}, pour un temps partiel de {1}, le nombre de RTT sera de => {2}")
     @CsvSource({
-            "2015-01-01, 1.0, 8",
+            "2015-01-01, 1.0, 9",
             "2019-01-01, 1.0, 8",
             "2021-01-01, 0.5, 5",
             "2022-01-01, 1.0, 10",
             "2032-01-01, 1.0, 11",
             "2039-01-01, 1.0, 10",
-            "2044-01-01, 1.0, 10",
+            "2044-01-01, 1.0, 9",
     })
     void testNbRTT(LocalDate dateAnnee, Double tempsPartiel, Integer result) {
         // Given
